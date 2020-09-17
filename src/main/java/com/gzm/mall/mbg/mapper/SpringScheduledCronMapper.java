73c188e90f5555ddec9a196456e5054ba2c8508a
甘之萌 <ZHIMENG.GAN@HAND-CHINA.COM>
2020-09-17 17:40:13 +0800

    [add] 新增定时任务

---
+++ src/main/java/com/gzm/mall/mbg/mapper/SpringScheduledCronMapper.java
@@ -0,0 +1,13 @@
+package com.gzm.mall.mbg.mapper;
+
+/**
+ * @author gzm
+ */
+//public interface SpringScheduledCronMapper extends BaseMapper<SpringScheduledCron> {
+//    /**
+//     * 根据cron_key查询
+//     * @param cronKey
+//     * @return
+//     */
+//    SpringScheduledCron findByCronKey(String cronKey);
+//}
